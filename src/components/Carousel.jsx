934fb2ae5c104f8313357e3c22ca5aefa345c556
adityach0uhan <m4with6x@gmail.com>
2023-08-18 23:02:25 +0530

    Design Changes

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,26 +3,23 @@ import '../styles/Carousel.css'
 const Carousel = () => {
     return (
         <div className='carousel-parent'>
-            <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="true">
+            <div className='input-box'>
+                <input type="text" className='mx-2' />
+                <button type='submit'><i class="fa-solid fa-magnifying-glass fa-shake"></i></button>
+            </div>
+            
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
                         <img src="images/Biryani.webp" class="d-block w-100" alt="..."/>
                     </div>
                     <div class="carousel-item">
-                        <img src="images/Burger.jpg" class="d-block w-100" alt="..."/>
-                    </div>
-                    <div class="carousel-item">
                         <img src="images/pasta.jpg" class="d-block w-100" alt="..."/>
                     </div>
+                    <div class="carousel-item">
+                        <img src="images/Burger.jpg" class="d-block w-100" alt="..."/>
+                    </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
             </div>
         </div>
     )
