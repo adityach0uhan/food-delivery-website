642ac0a11670bc50af8ba16af13fededbb5eaa1f
adityach0uhan <m4with6x@gmail.com>
2023-08-19 23:47:36 +0530

    Card added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Carousel from '../components/Carousel'
 import Card from '../components/Card'
 import '../styles/Home.css'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <div>
       <div>   <Carousel /> </div>
       <div className='Cards-container'> <Card/> </div>
-      
+      <div className='footer-container'><Footer/></div>
       
 
     </div>
