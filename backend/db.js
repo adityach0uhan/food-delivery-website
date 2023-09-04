e2ceb2a386bbd3caf60b8913f320e67b4984c5bd
adityach0uhan <m4with6x@gmail.com>
2023-09-04 16:04:55 +0530

    backend changes

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose" );
 require('dotenv').config();
 const uri = process.env.MongoDB_URI;
+// const uri = "mongodb://localhost:27017"
 
 // Connect to MongoDB using Mongoose
 function DataBaseConnect() {
+    console.log("Connnection in progress")
     mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected ');
