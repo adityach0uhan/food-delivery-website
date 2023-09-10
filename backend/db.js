cbc8939d8eaa1626a2b9b439865fb7ec9c694ee2
adityach0uhan <m4with6x@gmail.com>
2023-09-10 23:35:42 +0530

    Now the records are fetching

--- backend/db.js
+++ backend/db.js
@@ -6,24 +6,9 @@ async function DataBaseConnect() {
     console.log("Connnection in progress")
     await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(async () => {
-            const FetchedData = await mongoose.connection.db.collection("FoodList");
-            console.log('DataBase Connected ');
-            await FetchedData.find({}, async(err, resp) => {
-
-                if (err) { console.log("Error ") }
-
-                else {
-                   await resp.toArray((err, res) => {
-
-                        if (err) { console.log("err") }
-
-                        else { console.log(res) }
-                    })
-                }
-
-            })
-
-
+            const collection = mongoose.connection.db.collection('FoodList');
+            const result = await collection.find({}).toArray();
+            console.log(result);
         }).catch((err) => {
             console.error('Error connecting ', err);
         });
