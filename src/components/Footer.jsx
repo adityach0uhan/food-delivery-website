d020bd602f2e4b412b0b243496538ef08c06a40b
adityach0uhan <m4with6x@gmail.com>
2023-09-15 23:25:22 +0530

    Made some changes in Form

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
                     <section className="mb-4">
                         <a
                             className="btn btn-link btn-floating btn-lg text-dark m-1"
-                            href="#!"
+                            href="#"
                             role="button"
                             data-mdb-ripple-color="dark"
                         ><i className="fab fa-facebook-f"></i
@@ -16,7 +16,7 @@ const Footer = () => {
 
                         <a
                             className="btn btn-link btn-floating btn-lg text-dark m-1"
-                            href="#!"
+                            href="#"
                             role="button"
                             data-mdb-ripple-color="dark"
                         ><i className="fab fa-twitter"></i
@@ -24,7 +24,7 @@ const Footer = () => {
 
                         <a
                             className="btn btn-link btn-floating btn-lg text-dark m-1"
-                            href="#!"
+                            href="#"
                             role="button"
                             data-mdb-ripple-color="dark"
                         ><i className="fab fa-google"></i
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,28 +1,43 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return (
     <>
+      
+      <div className='container my-5 '>
 
-
-      <form>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      <form className='container m-2'>
+        <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+          <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
 
 
-        
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
         </div>
 
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Location</label>
+          <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Location" />
+        </div>
+
+        <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Name</label>
+          <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Name" />
+        </div>
+
+          <button type="submit" className="btn btn-primary">Submit</button>
+          <Link to="/login">
+          <button type="submit" className="btn btn-primary">login</button>
+          </Link>
       </form>
 
 
+      </div>
     </>
   )
 }
