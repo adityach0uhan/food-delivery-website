f9ef92e1d0c3fa0a175a769049fda9aa8831e49a
adityach0uhan <m4with6x@gmail.com>
2023-09-18 23:08:12 +0530

    form data submission functionality added

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,19 +3,23 @@ import { Link } from 'react-router-dom'
 
 const SignUp = () => {
 
-  const [formData, setformData] = useState({name:"",email:"",password:"",location:""});
+  const [formData, setformData] = useState({ name: "", email: "", password: "", location: "" });
 
   const handelSubmit = async (e) => {
     e.preventDefault();
-    
-    const backendData = await fetch("http://localhost:5000/db/createUser", {
-      method: 'POST',
-      headers: {
-        'Content-Type':'application/json'
-      },
-      body:JSON.stringify()
-    })
 
+    // const backendData = await fetch("http://localhost:5000/db/createUser", {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify()
+    // })
+    console.log(formData)
+    // setformData("")
+  }
+  const onchange = (e) => {
+    setformData({ ...formData, [e.target.name]: e.target.value })
   }
 
   return (
@@ -28,7 +32,7 @@ const SignUp = () => {
 
             <label htmlFor="exampleInputEmail1">Email address</label>
 
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value={formData.email} name="email" />
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value={formData.email} name="email" onChange={onchange} />
 
             <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
 
@@ -37,22 +41,22 @@ const SignUp = () => {
 
           <div className="form-group">
             <label htmlFor="exampleInputPassword1">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" value={formData.password} />
+            <input type="password" className="form-control"  placeholder="Password" name='password' onChange={onchange} value={formData.password} />
           </div>
 
           <div className="form-group">
             <label htmlFor="exampleInputPassword1">Location</label>
-            <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Location" value={formData.location} />
+            <input type="text" className="form-control"  placeholder="Location" name='location' onChange={onchange} value={formData.location} />
           </div>
 
           <div className="form-group">
             <label htmlFor="exampleInputPassword1">Name</label>
-            <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Name" value={formData.name} />
+            <input type="text" className="form-control"  placeholder="Name" name='name' onChange={onchange} value={formData.name} />
           </div>
 
-          <button type="submit" className="btn btn-primary">Submit</button>
+          <button type="submit" className="btn btn-primary m-3">Submit</button>
           <Link to="/login">
-            <button type="submit" className="btn btn-primary">login</button>
+            <button type="submit" className="btn btn-primary m-3">login</button>
           </Link>
         </form>
 
